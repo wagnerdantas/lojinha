4340477c23f6182297ee758df20d318139564414
wagnerdantas <wagner.trc@gmail.com>
2023-03-13 15:19:43 -0300

    Atualização

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,8 +1,18 @@
 package br.com.lojinha;
 
+import br.com.lojinha.pojo.Produto;
+
 public class LojinhaApp {
     public static void main(String[] args) {
-        System.out.println("Lojinha");
+        Produto meuProduto = new Produto();
+
+        meuProduto.nome = "Playstation";
+        meuProduto.valor = 5000;
+        meuProduto.marca = "Sony";
+        meuProduto.tamanho = "Médio";
+        meuProduto.itensInclusos = "2 Controles e 1 Jogo";
+
+        System.out.println();
 
     }
 }
