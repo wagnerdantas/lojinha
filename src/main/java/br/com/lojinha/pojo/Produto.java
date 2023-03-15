112f727cea67ddd12a3476b20abee8c5cab94d1e
wagnerdantas <wagner.trc@gmail.com>
2023-03-14 23:18:21 -0300

    Atualização

--- src/main/java/br/com/lojinha/pojo/Produto.java
+++ src/main/java/br/com/lojinha/pojo/Produto.java
@@ -2,9 +2,59 @@ package br.com.lojinha.pojo;
 
 public class Produto {
 
-    public String nome;
-    public String marca;
-    public double valor;
-    public String tamanho;
-    public String itensInclusos;
+    private String nome;
+    private String marca;
+    private double valor;
+    private String tamanho;
+    private String itensInclusos;
+    public String getNome() {
+        return nome;
+    }
+    //Constructor
+    public void Produto(){
+        this.nome = nome;
+        this.marca = marca;
+        this.valor = valor;
+        this.tamanho = tamanho;
+        this.itensInclusos= itensInclusos;
+    }
+
+    //Getters and Setter
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getItensInclusos() {
+        return itensInclusos;
+    }
+
+    public void setItensInclusos(String itensInclusos) {
+        this.itensInclusos = itensInclusos;
+    }
+
+
 }
